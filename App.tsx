b7a67c7d708218954f7546de94881f7dbc4927a5
bakhtaouiines <inesbkht@gmail.com>
2022-05-31 11:59:36 +0200

    navigation

--- App.tsx
+++ App.tsx
@@ -1,67 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import * as React from 'react';
+import { Image, Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './src/Screens/Login';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  ActivityIndicator,
-} from 'react-native';
+// function HomeScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen</Text>
+//       <Button
+//         title="Go to Details"
+//         onPress={() => navigation.navigate('Details')}
+//       />
+//     </View>
+//   );
+// }
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input, Image } from "@rneui/themed";
+// function DetailsScreen({ navigation }) {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Details Screen</Text>
+//       <Button
+//         title="Go to Details... again"
+//         onPress={() => navigation.push('Details')}
+//       />
+//       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+//       <Button title="Go back" onPress={() => navigation.goBack()} />
+//       <Button
+//         title="Go back to first screen in stack"
+//         onPress={() => navigation.popToTop()}
+//       />
+//     </View>
+//   );
+// }
 
-const App = () => {
-
+function LogoTitle() {
   return (
-    <SafeAreaProvider>
-      <View style={styles.container}>
-        <Image
-          source={require('./public/images/logo_immopport1.webp')}
-          containerStyle={styles.image}
-        />
-      </View>
-
-      <Input placeholder='Email' />
-      <Input placeholder='Mot de passe' />
-
-    </SafeAreaProvider>
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={require('./Assets/images/logo_immopport1.webp')}
+    />
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
+const Stack = createNativeStackNavigator();
 
-  image: {
-    aspectRatio: 1,
-    width: '100%',
-    textAlign: "center",
-    marginTop: 16,
-    paddingVertical: 8,
-  },
-});
+function App() {
+  return (
+    <NavigationContainer>
+          <Image
+      style={{ width: 50, height: 50 }}
+      source={require('./Assets/images/logo_immopport1.webp')}
+    />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="LoginScreen" component={LoginScreen}/>
+        {/* <Stack.Screen name="Home" component={HomeScreen} options={{ headerTitle: (props) => <LogoTitle {...props} /> }} />
+        <Stack.Screen name="Details" component={DetailsScreen} options={{ headerTitle: (props) => <LogoTitle {...props} /> }} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
