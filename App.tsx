34eee1d0fbebb17333d0340459df6ef0cfc575d1
bakhtaouiines <inesbkht@gmail.com>
2022-06-01 15:21:36 +0200

    menu updates

--- App.tsx
+++ App.tsx
@@ -1,64 +1,29 @@
-import * as React from 'react';
-import { Image, Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './src/Screens/Login';
+import * as React from 'react'
+import { Image, Button, View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import LoginScreen from './src/Screens/LoginScreen'
+import HomeScreen from './src/Screens/HomeScreen'
+import AuthContext from './src/Contexts/AuthContext'
+import CalendarScreen from './src/Screens/CalendarScreen'
 
-// function HomeScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>Home Screen</Text>
-//       <Button
-//         title="Go to Details"
-//         onPress={() => navigation.navigate('Details')}
-//       />
-//     </View>
-//   );
-// }
-
-// function DetailsScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>Details Screen</Text>
-//       <Button
-//         title="Go to Details... again"
-//         onPress={() => navigation.push('Details')}
-//       />
-//       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-//       <Button title="Go back" onPress={() => navigation.goBack()} />
-//       <Button
-//         title="Go back to first screen in stack"
-//         onPress={() => navigation.popToTop()}
-//       />
-//     </View>
-//   );
-// }
-
-function LogoTitle() {
-  return (
-    <Image
-      style={{ width: 50, height: 50 }}
-      source={require('./Assets/images/logo_immopport1.webp')}
-    />
-  );
-}
-
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 function App() {
+  const user = React.useContext(AuthContext)
   return (
     <NavigationContainer>
-          <Image
-      style={{ width: 50, height: 50 }}
-      source={require('./Assets/images/logo_immopport1.webp')}
-    />
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="LoginScreen" component={LoginScreen}/>
-        {/* <Stack.Screen name="Home" component={HomeScreen} options={{ headerTitle: (props) => <LogoTitle {...props} /> }} />
-        <Stack.Screen name="Details" component={DetailsScreen} options={{ headerTitle: (props) => <LogoTitle {...props} /> }} /> */}
+      <Image
+        style={{ width: 50, height: 50, alignSelf: "center" }}
+        source={require('./Assets/images/logo.webp')}
+      />
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Connexion' }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Bonjour' }} />
+        <Stack.Screen name="Calendar" component={CalendarScreen} options={{ title: 'Calendrier' }} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
-export default App;
+export default App
