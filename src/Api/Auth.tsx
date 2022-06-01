5194ca6e01017b5be9d69ddef34f865168221776
bakhtaouiines <inesbkht@gmail.com>
2022-06-01 15:20:33 +0200

    interfaces + auth context + home & calendar screens

--- src/Api/Auth.tsx
+++ src/Api/Auth.tsx
@@ -1,21 +1,37 @@
-import { AsyncStorage } from "react-native";
-import customAxios from "./BaseUrl";
+import customAxios from "./BaseUrl"
+import { MMKVLoader } from "react-native-mmkv-storage"
 
-export const login = async (idNumber: string, password: string) => {
-    return customAxios.get('authentification', {
+const MMKV = new MMKVLoader().withEncryption()
+.encryptWithCustomKey("kjdhsvcjhqdsjvhdjsgxfcgh", true, "myToken")
+.initialize()
+
+export const login = async (matricule: number, password: string) => {
+    return customAxios.get('authentification/employee', {
         params: {
-            idNumber,
+            matricule,
             password
         }
 
     }).then((response) => {
         if (response.data) {
-            console.log(response)
-            AsyncStorage.setItem("access_token", response.data.access_token);
+            MMKV.setStringAsync("access_token",response.data.access_token)
         }
-        return response;
+        return response
     }).catch(error => {
-        return error.response;
-    });
+        return error.response
+    })
+}
 
-};
+export const getProfile = async (token: string) => {
+    return customAxios.get("espaceemploye/dashboard", {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+        .then((response) => {
+            return response.data.employee
+        })
+        .catch((error) => {
+            console.error("Error", error)
+        })
+}
---
+++ src/Interfaces/Employee.tsx
@@ -0,0 +1,10 @@
+interface Employee {
+    id_agency: number | undefined;
+    lastname: string | undefined;
+    firstname: string | undefined;
+    mail: string | undefined;
+    phone: number | undefined;
+    matricule: number | undefined;
+}
+
+export default Employee
