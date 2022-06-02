7369d83235c5872ff4c0e708a844631ead32b1be
Taslima-Ahamed-Mze <taslimaahamedmze@gmail.com>
2022-06-02 15:22:17 +0200

    Button add new prospect

--- src/Screens/ProspectionScreen.tsx
+++ src/Screens/ProspectionScreen.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Header,Card, Icon } from "@rneui/themed";
-import { Image, Button, View, Text, ScrollView ,StyleSheet} from 'react-native';
+import { Image, Button, View, Text, ScrollView ,StyleSheet, Dimensions, TouchableHighlight} from 'react-native';
 import AuthContext from '../Contexts/AuthContext';
 
 import HeaderComponent from '../Components/Header';
 import CardDashboard from '../Components/CardDashboard';
 import FooterComponent from '../Components/Footer';
+import { color } from '@rneui/base';
 
 
 const styles = StyleSheet.create({
@@ -18,7 +19,13 @@ const styles = StyleSheet.create({
         color: "black",
         fontSize: 16
 
-    }
+    },
+    circle: {
+        width: 44,
+        height: 44,
+        borderRadius: 44/2,
+        color:'red'
+     }
 })
 
 const ComponentProspect = ()=>{
@@ -37,7 +44,7 @@ const ComponentProspect = ()=>{
 
 
 
-const Prospections = () => {
+const Prospections = ({ navigation }: any) => {
 
     return(
 
@@ -51,17 +58,33 @@ const Prospections = () => {
                 <ComponentProspect />
                 <ComponentProspect />
                 <ComponentProspect />
-                <ComponentProspect />
-                <ComponentProspect />
-                <ComponentProspect />
-                <ComponentProspect />
-                <ComponentProspect />
-                <ComponentProspect />
-                <ComponentProspect />
+                
 
-                <ComponentProspect />
+                
+                
 
             </ScrollView>
+            <TouchableHighlight
+                style = {{
+                    borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+                    width: 50,
+                    height: 50,
+                    backgroundColor:'#e2e2e2',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginLeft:330,
+                    marginBottom:13
+                }}
+                underlayColor = '#ccc'
+                onPress = { () => navigation.navigate('Home') }
+            >
+                <Icon
+                    name='add'
+                    color='#ef3a5d' 
+                    size={40}
+                />
+      
+            </TouchableHighlight>
             <View>
                 <FooterComponent />
             </View>
