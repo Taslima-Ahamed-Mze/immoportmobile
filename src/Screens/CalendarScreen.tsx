e64e919dbb3003efd1a679a192fc157193ce61fc
Bastien Daugenne <bastien.daugenne@gmail.com>
2022-06-02 16:58:59 +0200

    Start to work on the Calendar screen

--- src/Screens/CalendarScreen.tsx
+++ src/Screens/CalendarScreen.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import * as React from 'react';
 import { View, Button,Text, ScrollView} from 'react-native';
+import {useState, useEffect} from 'React';
 import HeaderComponent from '../Components/Header';
-import FooterComponent from '../Components/Footer';
+import FooterComponent from '../Components/Footer'; 
 
 const CalendarScreen = ({ navigation }: any) => {
+
+    //String array storing the names of the months 
+    const months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+                    'Juillet', 'Août', 'Septembre', 'Novembre', 'Décembre'];
+
+    //String array storing the names of the days in a week
+    const weekDays = ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam','Dim'];
+
+    //String array storing the number of days for each months
+    const numberMonthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+   const [date, setDate] = useState();
+
 
     return (
         <View style={{ flex: 1,padding:10}}>
