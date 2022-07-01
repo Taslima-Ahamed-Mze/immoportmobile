6e6cc93650a7c7d3fae91a954a3cc8615ae76b20
Taslima-Ahamed-Mze <taslimaahamedmze@gmail.com>
2022-07-01 17:02:38 +0200

    debug Property Interface

--- src/Interfaces/Property.tsx
+++ src/Interfaces/Property.tsx
@@ -1,13 +1,16 @@
 interface Property {
-    name: string | undefined
-    price: string 
-    address: string | undefined
-    addition_address: string | undefined
-    zipcode: string | undefined
-    city: string | undefined
-    description: string | undefined
-    surface: string | undefined
-    isFurnished: boolean | undefined
+    property:{
+        name:string | undefined
+        price: string 
+        address: string | undefined
+        addition_address: string | undefined
+        zipcode: string | undefined
+        city: string | undefined
+        description: string | undefined
+        surface: string | undefined
+        isFurnished: boolean | undefined
+    };
+    id: number | undefined
 }
 
 export default Property
